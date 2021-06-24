9b20d7bfc91078c2a64123d94cef13ef9e2bd904
Keith Carter <keith.carter@yahoo.com>
2021-06-24 22:08:04 +0200

    lots of stuff, not qutie sure, but need to push

--- public/javascripts/drawing.js
+++ public/javascripts/drawing.js
@@ -36,7 +36,8 @@ export async function drawMap(polylines,duration,fps) {
 		container: 'map',
 		style: 'mapbox://styles/mapbox/streets-v11',
 		interactive: false,
-		fadeDuration: 0
+		fadeDuration: 0,
+		preserveDrawingBuffer: true
 	});
 	/*
 	map.on('render', function() {
@@ -53,29 +54,147 @@ export async function drawMap(polylines,duration,fps) {
 	});
 	*/
 	map.fitBounds([
-			[minLng,minLat],
-			[maxLng,maxLat]
+		[minLng,minLat],
+		[maxLng,maxLat]
 		],
 		// the padding makes the map a little less cut close at the edges
 		{
 			duration: 0,
-			padding: 40
+			padding: 60
 		}
 	);
 
-	// same as: map.on('load', async function() {
+
 	await new Promise(resolve => map.on('load', resolve));
+
+	const images =[
+		{url: '/images/green-flag-48.png', id: 'image_1'},
+		{url: '/images/finish-flag-48.png', id: 'image_2'},
+	]
+
+/*
+	Promise.all(
+		images.map(img => new Promise((resolve, reject) => {
+			map.loadImage(img.url, function (error, res) {
+				map.addImage(img.id, res)
+				resolve();
+			})
+		}))
+	)
+*/
+	/*
+	await new Promise((resolve, reject) => {
+		map.loadImage('/images/green-flag-48.png', function (error, res) {
+			// Add the image to the map style.
+			map.addImage('green-flag-48', res);
+			
+			const startPoint = {
+				'type': 'FeatureCollection',
+				'features': [
+					{
+						'type': 'Feature',
+						'geometry': {
+							'type': 'Point',
+							'coordinates': decodedPolylines[0][0]
+						},
+						'properties': {
+							//'bearing': 90
+						}
+					}
+				]
+			}
+
+			// Add a data source containing one point feature.
+			map.addSource('start-source', {
+				'type': 'geojson',
+				'data': startPoint
+			});
+
+			// Add a layer and use the image to represent the data.
+			map.addLayer({
+				'id': 'start-flag',
+				'type': 'symbol',
+				'source': 'start-source', // reference the data source
+				'layout': {
+					'icon-image': 'green-flag-48', // reference the image
+					'icon-offset': [16,-24],
+					//'icon-size': 2.5,
+					'icon-allow-overlap': true,
+					'text-allow-overlap': true,
+					'icon-ignore-placement': true,
+					'text-ignore-placement': true,
+					//'icon-rotate': ['get', 'bearing'],
+					//'icon-rotation-alignment': 'map',
+					//'icon-keep-upright': true
+				}
+			});
+
+			resolve();
+		});
+	});
+
+	await new Promise((resolve, reject) => {
+		map.loadImage('/images/finish-flag-48.png', function (error, res) {
+			// Add the image to the map style.
+			map.addImage('finish-flag-48', res);
+			
+			const endPoint = {
+				'type': 'FeatureCollection',
+				'features': [
+					{
+						'type': 'Feature',
+						'geometry': {
+							'type': 'Point',
+							'coordinates': decodedPolylines[decodedPolylines.length - 1][decodedPolylines[decodedPolylines.length - 1].length - 1]
+						},
+						'properties': {
+							//'bearing': 90
+						}
+					}
+				]
+			}
+
+			// Add a data source containing one point feature.
+			map.addSource('finish-source', {
+				'type': 'geojson',
+				'data': endPoint
+			});
+
+			// Add a layer and use the image to represent the data.
+			map.addLayer({
+				'id': 'finish-flag',
+				'type': 'symbol',
+				'source': 'finish-source', // reference the data source
+				'layout': {
+					'icon-image': 'finish-flag-48', // reference the image
+					'icon-offset': [6,-24],
+					//'icon-size': 2.5,
+					'icon-allow-overlap': true,
+					'text-allow-overlap': true,
+					'icon-ignore-placement': true,
+					'text-ignore-placement': true,
+					//'icon-rotate': ['get', 'bearing'],
+					//'icon-rotation-alignment': 'map',
+					//'icon-keep-upright': true
+				}
+			});
+
+			resolve();
+		});
+	});
+	*/
+
 	for (let i = 0; i < decodedPolylines.length; i++) {
 		geoJsons[i] = {
 			'type': 'FeatureCollection',
 			'features': [
-			{
-				'type': 'Feature',
-				'geometry': {
-					'type': 'LineString',
-					'coordinates': []
+				{
+					'type': 'Feature',
+					'geometry': {
+						'type': 'LineString',
+						'coordinates': []
+					}
 				}
-			}
 			]
 		};
 
@@ -92,14 +211,17 @@ export async function drawMap(polylines,duration,fps) {
 				'line-cap': 'round'
 			},
 			'paint': {
-				'line-opacity': 0.3,
+				'line-opacity': 0.5,
 				'line-color': colors[i],
-				'line-width': 4
+				'line-width': 6
 			}
 		});
 	}
 
+
+	await new Promise(resolve => map.on('render', resolve));
 	await new Promise(resolve => map.on('idle', resolve));
+
 	progress(5);
 	// initialize H264 video encoder
 	const Encoder = await loadEncoder();
@@ -119,7 +241,7 @@ export async function drawMap(polylines,duration,fps) {
 	progress(10);
 
 	let pointsDrawn = 0;
-	const totalFrames = (duration - 2000) * fps / 1000;
+	const totalFrames = (duration - 3000) * fps / 1000;
 	const ptr = encoder.getRGBPointer(); // keep a pointer to encoder in WebAssembly heap memory
 
 	// reads the pixels from the canvas and places them into the encoder
@@ -145,24 +267,14 @@ export async function drawMap(polylines,duration,fps) {
 	async function animate() {
 		for(let i = 1; i <= totalFrames; i++) {
 			// drawFrame awaits the render event from mapbox
-			await drawFrame(totalFrames, i, totalPoints, decodedPolylines, geoJsons, map);
+			await drawFrame(totalFrames, i, totalPoints, decodedPolylines, geoJsons, map, null);
 			encodeFrame();
 			if (i % 5 == 0) progress( 10 + Math.ceil(90 * i / totalFrames) );
 		}
 	}
 
-	/* I HAVE NO IDEA WHY I NEED THIS */
-	// but if i don't include it the first frames are black
-	map.easeTo({
-		bearing: map.getBearing(),
-		duration: 1,
-		easing: t => t
-	});
-	await new Promise(resolve => map.on('moveend', resolve));
-	/* /I HAVE NO IDEA WHY I NEED THIS */
-
 	// create a one second buffer at the beginning of the video	
-	for(let i = 1; i <= fps; i++) {
+	for(let i = 1; i <= fps * 1; i++) {
 		encodeFrame();
 	}
 
@@ -172,7 +284,8 @@ export async function drawMap(polylines,duration,fps) {
 	console.log('viewTime, readTime, encodeTime',viewTime, readTime, encodeTime);
 	//console.log('Runtime:',performance.now()-start);
 
-	for(let i = 1; i <= fps; i++) {
+	// create a two second buffer at the end of the video
+	for(let i = 1; i <= 2 * fps; i++) {
 		encodeFrame();
 	}
 
@@ -198,7 +311,7 @@ totalFrames: the total number of frames in this animation
 thisFrameNumber: the number of this frame
 pointsDrawn: how many points have been drawn so far
 */
-async function drawFrame(totalFrames, thisFrameNumber, totalPoints, decodedPolylines, geoJsons, map) {
+async function drawFrame(totalFrames, thisFrameNumber, totalPoints, decodedPolylines, geoJsons, map, point) {
 	// no points have been drawn yet on the 0 or 1 frame
 	let pointsDrawn;
 	if (thisFrameNumber <= 1) {
@@ -232,13 +345,21 @@ async function drawFrame(totalFrames, thisFrameNumber, totalPoints, decodedPolyl
 			lengthsSum += decodedPolylines[i].length;
 			i++;
 		// we're missing at least some (and maybe all) of this segment
-		} else {
-			if (decodedPolylines[i].length - leftoverPoints > pointsToDraw) {
+	} else {
+		if (decodedPolylines[i].length - leftoverPoints > pointsToDraw) {
 				// all the remaining points to draw can be drawn from this polyline without using all of the polyline
 				theSlice = decodedPolylines[i].slice(leftoverPoints,leftoverPoints + pointsToDraw);
 				geoJsons[i].features[0].geometry.coordinates.push(...theSlice);
 				map.getSource('route-' + i).setData(geoJsons[i]);
 				pointsDrawn += pointsToDraw;
+
+				/*
+				// modify the bearing
+				point.features[0].properties.bearing = turf.bearing(
+					turf.point(decodedPolylines[i][leftoverPoints + pointsToDraw]), // start
+					turf.point(decodedPolylines[i][leftoverPoints + pointsToDraw - 1])  // end
+				);
+				*/
 				pointsToDraw = 0;
 			} else {
 				// can use everything in this polyline (and perhaps some of the next, which we'll check on next loop)
@@ -250,6 +371,8 @@ async function drawFrame(totalFrames, thisFrameNumber, totalPoints, decodedPolyl
 				lengthsSum += decodedPolylines[i].length;
 				i++;
 			}
+			//point.features[0].geometry.coordinates = theSlice[theSlice.length - 1];
+			//map.getSource('point').setData(point);
 		}
 	}
 	// important: do not return until render is complete and map reports idle
--- public/javascripts/utility.js
+++ public/javascripts/utility.js
@@ -1,3 +1,21 @@
+function promisify(f) {
+  return function (...args) { // return a wrapper-function (*)
+    return new Promise((resolve, reject) => {
+      function callback(err, result) { // our custom callback for f (**)
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      }
+
+      args.push(callback); // append our custom callback to the end of f arguments
+
+      f.call(this, ...args); // call the original function
+    });
+  };
+}
+
 // https://stackoverflow.com/questions/19491336/how-to-get-url-parameter-using-jquery-or-plain-javascript
 var getUrlParameter = function getUrlParameter(sParam) {
 	var sPageURL = window.location.search.substring(1),
